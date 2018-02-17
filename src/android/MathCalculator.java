4d6418ea78f616aab350faabcd23242b026c0896
Jayesh Lahare <jayesh.indore@gmail.com>
2018-02-17 17:05:10 +0530

    Minor fixes

--- src/android/MathCalculator.java
+++ src/android/MathCalculator.java
@@ -35,7 +35,7 @@ public class MathCalculator extends CordovaPlugin {
         {
                 try
                 {
-                     int p1 = Integer.paseInt(args.getJSONObject(0).getString("param1"));
+                     int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
                      int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
                        
                      callback.success(""+ (p1+p2) );
@@ -57,7 +57,7 @@ public class MathCalculator extends CordovaPlugin {
         {
                 try
                 {
-                     int p1 = Integer.paseInt(args.getJSONObject(0).getString("param1"));
+                     int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
                      int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
                        
                      callback.success(""+ (p1-p2) );
